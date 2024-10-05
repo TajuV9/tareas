4b06e34b6177fb9e7205c0b679f573a357229ec5
Cristian <131365312+TajuV9@users.noreply.github.com>
2024-10-05 12:43:56 +0200

    Update funcionesPPT.php

--- funcionesPPT.php
+++ funcionesPPT.php
@@ -1,12 +1,17 @@
 <?php 
+    // Meto los movimientos en un array
+    function meterMovimientos(){
+        return [
+            PIEDRA1,
+            PIEDRA2,
+            TIJERAS,
+            PAPEL
+        ];
+    }
+
     // Elegir piedra, papel o tijera y devuelvo el movimiento elegido al azar
-    function eleccionMovimiento(){
-        switch(random_int(1,4)) {
-            case 1: return PIEDRA1; 
-            case 2: return PIEDRA2; 
-            case 3: return TIJERAS; 
-            case 4: return PAPEL; 
-        }
+    function eleccionMovimiento($jugadas){
+        return $jugadas[random_int(0,3)];
     }
 
     // En función del movimiento de cada jugador se elige al ganador
@@ -17,18 +22,15 @@
             ($j1 == PIEDRA1 && $j2 == TIJERAS) || 
             ($j1 == PAPEL && $j2 == PIEDRA2) || 
             ($j1 == TIJERAS && $j2 == PAPEL)
-            )
-        {
+            ) {
             return 1;
-        } else {
-            return 2;
-        } 
+        } else return 2;
     }
 
     // Construyo el mensaje de empate o ganador correspondiente
     function construirMensaje($vencedor){
-        return ($vencedor == EMPATE) ? 
-        "¡Empate!" : 
-        "Ha ganado el jugador " . ($vencedor == GANA1 ? GANA1 : GANA2);
+        return ($vencedor == EMPATE) ? "¡Empate!" : 
+        "Ha ganado el jugador " . 
+        ($vencedor == GANAJUGADOR1 ? GANAJUGADOR1 : GANAJUGADOR2);
     }
 ?>
