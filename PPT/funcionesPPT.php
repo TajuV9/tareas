b3c715ed55826bfd9e6df553993e2328a17f00e8
Cristian <131365312+TajuV9@users.noreply.github.com>
2024-10-06 19:35:22 +0200

    Update funcionesPPT.php

--- PPT/funcionesPPT.php
+++ PPT/funcionesPPT.php
@@ -6,15 +6,11 @@
 
     // En función del movimiento de cada jugador se elige al ganador
     function elegirGanador($j1, $j2){
-        if ($j1 == $j2 || $j1 == PIEDRA1 && $j2 == PIEDRA2) {
-            return 0;
-        } else if (
-            ($j1 == PIEDRA1 && $j2 == TIJERAS) || 
-            ($j1 == PAPEL && $j2 == PIEDRA2) || 
-            ($j1 == TIJERAS && $j2 == PAPEL)
-            ) {
-            return 1;
-        } else return 2;
+        if ($j1 == $j2 || $j1 == PIEDRA1 && $j2 == PIEDRA2) return 0;
+        else if ($j1 == PIEDRA1 && $j2 == TIJERAS || 
+                $j1 == PAPEL && $j2 == PIEDRA2 || 
+                $j1 == TIJERAS && $j2 == PAPEL) return 1;
+        else return 2;
     }
 
     // Construyo el mensaje de empate o ganador correspondiente
