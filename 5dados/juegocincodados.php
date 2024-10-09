4d24bfa66eda35ef598248d7f300e689459eddea
Cristian <131365312+TajuV9@users.noreply.github.com>
2024-10-09 20:12:31 +0200

    Update juegocincodados.php

--- 5dados/juegocincodados.php
+++ 5dados/juegocincodados.php
@@ -1,1 +1,21 @@
+<?php
 
+    require_once(funciones5Dados.php);
+
+    define('UNO', "&#9856");
+    define('DOS', "&#9857");
+    define('TRES', "&#9858");
+    define('CUATRO', "&#9859");
+    define('CINCO', "&#9860");
+    define('SEIS', "&#9861");
+    $numerosDado = [UNO, DOS, TRES, CUATRO, CINCO, SEIS];
+
+    $tiradas1 = elegirTiradas($numerosDado);
+    $puntuacion1 = sumarPuntuacion($tiradas1);
+
+    $tiradas2 = elegirTiradas($numerosDado);
+    $puntuacion2 = sumarPuntuacion($tiradas2);
+    
+    $resultado = quienGana($puntuacion1, $puntuacion2);
+
+?>
